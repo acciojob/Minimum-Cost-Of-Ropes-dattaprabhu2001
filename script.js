73ba7fabb7a176dba0243ebc072230cec3d15f8a
dattaprabhu2001 <99395413+dattaprabhu2001@users.noreply.github.com>
2023-02-22 05:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-//your code here
-
-// function minCostToFormRope(a) {
-//     a.preventDefault();
-// var inputValue = document.querySelector("input").value;
-//     var arr = inputValue.split(",");
-//     arr.sort(function (a, b) {
-//         return a - b;
-//     })
-//     var cost = 0;
-//     while (arr.length > 1) {
-//         var res = Number(arr[0]) + Number(arr[1]);
-//         cost += res;
-//         arr.push(res);
-//         arr.unshift();
-//         arr.unshift();
-//          arr.sort(function (a, b) {
-//         return a - b;
-//     })
-
-
-//     }
-//     document.getElementById('result').textContent = cost;
-
-
-
-// }
-
 function minCostToFormRope(a) {
   a.preventDefault();
   var inputElement = document.querySelector("input").value;
